b08974ca7c455189ea1506f8008e00cc9f951f4c
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-03-12 12:11:54 +0300

    Update

--- index.js
+++ index.js
@@ -53,16 +53,14 @@ app.post("/webhook", (req, res) => {
     let messageToCustomer;    
     const text = textMessage.replace(keyword, '').trim();//remove "Key Word" 
     const time = new Date();
-    sql.connect(config, function(err, connection) {
-        if (err) {
-          console.error('Error connecting to database: ' + err.stack);
-          return;
-        }
-        console.log('Connected to database');
-      //chack if user exists
+    let status = '';
+    let messagingStep = '';
+
+    function handleIncomingMessage(text, sender) {
+        // Check if user exists in database
         const checkIfExistsQuery = "SELECT TOP 1 * FROM two_way_sms_tb WHERE phoneNumber = @phoneNumber AND isActive = 1";
         const checkIfExistsRequest = new sql.Request(connection);
-        checkIfExistsRequest.input('phoneNumber', sql.VarChar, phoneNumber);
+        checkIfExistsRequest.input('phoneNumber', sql.VarChar, sender);
         checkIfExistsRequest.query(checkIfExistsQuery, function(checkErr, checkResults) {
           if (checkErr) {
             console.error('Error executing checkIfExistsQuery: ' + checkErr.stack);
@@ -71,196 +69,199 @@ app.post("/webhook", (req, res) => {
           }
           if (checkResults.recordset.length > 0) {
             console.log('User already exists');
-            let status = checkResults.recordset[0].status;
-            let messagingStep = checkResults.recordset[0].messagingStep;
-            console.log(status);
-            console.log(messagingStep);
-            sql.close();
-            return;
-          }
-      
-          const insertQuery = "INSERT INTO two_way_sms_tb (text, text_id_AT, phoneNumber, isActive, time) VALUES (@text, @textId, @phoneNumber, @isActive, @time)";
-          const insertRequest = new sql.Request(connection);
-          insertRequest.input('text', sql.VarChar, text);
-          insertRequest.input('textId', sql.VarChar, textId);
-          insertRequest.input('phoneNumber', sql.VarChar, phoneNumber);
-          insertRequest.input('isActive', sql.Bit, isActive);
-          insertRequest.input('time', sql.DateTime2, time);
-          insertRequest.query(insertQuery, function(insertErr, insertResults) {
-            if (insertErr) {
-              console.error('Error executing insertQuery: ' + insertErr.stack);
-              sql.close();
-              return;
-            }
-            console.log('INSERT successful');
-            sql.close();
-          });
-        });
-      });
-      
-      
-    if(!isRegistering && !isDeleting && !isCheckingAccount && !ResetingPassword){
-        switch (text.toLowerCase()) {
-            // case '':
-            case 'register':
-                //reset isRegistering flag and registrationStep
-                isRegistering = false;
-                registrationStep = 0;
-                sms.send(register.newCustomer(sender));
-                sms.send(register.enterId(sender));
-                //set a flag to indicate that the user is in the process of registering
-                isRegistering = true;         
-                //request for ID number
-                registrationStep = 2;
-                const status = "isRegistering";
-                const phoneNumber = sender;
-                const messagingStep= "2";
-                sql.connect(config, function(err) {
-                    const request = new sql.Request();
-                    const updateRegister1 = `UPDATE two_way_sms_tb SET status = @status, messagingStep = @messagingStep WHERE phoneNumber = @phoneNumber AND time = (
-                        SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumber )`;
-                    request.input('status', sql.VarChar, status);
-                    request.input('messagingStep', sql.VarChar, messagingStep);
-                    request.input('phoneNumber', sql.VarChar, phoneNumber);
-                    request.query(updateRegister1, function(err, results) {
-                      if (err) {
-                        console.error('Error executing query: ' + err.stack);
-                        return;
-                      }
-                      console.log('UPDATE successful');
-                      sql.close();
-                    });
-                  });
+            const status = checkResults.recordset[0].status;
+            const messagingStep = checkResults.recordset[0].messagingStep;
+            switch (status) {
+              case 'isRegistering':
+                handleRegister(text, sender, messagingStep);
                 break;
-        ///other Cases
-                default:
-                    messageToCustomer = 'Welcome To Octagon Africa you can access our services by sending the word register,save, balance,statement,products';
-                    sms.send({
-                        to: sender,
-                        from:'20880',
-                        message: messageToCustomer
-                    });
-                    break;
+              case 'isDeleting':
+                handleDelete(text, sender, messagingStep);
+                break;
+              case 'isCheckingAccount':
+                handleAccountCheck(text, sender, messagingStep);
+                break;
+              case 'ResetingPassword':
+                handlePasswordReset(text, sender, messagingStep);
+                break;
+              default:
+                console.log('Unknown status: ' + status);
+                break;
             }
-        }else if(isRegistering) {
-            switch (registrationStep) {
-                case 1:
-                    // request for ID number  
-                    sms.send(register.enterId(sender));
-                    registrationStep = 2;
-                    break;
-                case 2:
-                    // process ID number and request for county
-                    if(validateId(text)) {
-                        user = user ? {...user, id: text} : {id: text};            
-                        sms.send(register.enterEmail(sender));
-                        registrationStep = 3;
-                    } else {
-                        messageToCustomer = "Invalid ID number. Please enter a valid 6-digit ID number";
-                        registrationStep = 1;
-                    }
-  
-                    break;
-                case 3:
-                    //request 6 character password
-                    user.email=text;        
-                    sms.send(register.enterPassword(sender));
-                    registrationStep = 4;
-                    break;
-                case 4:
-                    //request for fname
-                    user.password=text;
-                    //validate password             
-                    sms.send(register.enterFirstName(sender));
-                    registrationStep = 5;
-                    break;
-                case 5:
-                    //request for lname
-                    user.firstname=text;             
-                    sms.send(register.enterLastName(sender));
-                    registrationStep = 6;
-                    break;
-                case 6:
-                    // process full name and send confirmation message
-                    user.lastname=text;
-                        // Sending the request to octagon registration API
-                        var client = new Client();
-                        // set content-type header and data as json in args parameter
-                        var args = {
-                            data: { firstname: user.firstname, lastname: user.lastname, ID: user.id, email: user.email, password: user.password, phonenumber: sender },
-                            headers: { "Content-Type": "application/json" }
-                        };
-                        // Actual Octagon user registration API
-                        client.post("https://api.octagonafrica.com/v1/register", args, function (data, response) {
-                            // parsed response body as js object
-                            console.log(data);
-                            if ([200].includes(response.statusCode)) {
-                                // success code
-                                sms.send({
-                                    to: sender,
-                                    from:'20880',
-                                    message: "Congratulations!! "+user.firstname.toUpperCase() + " "+ user.lastname.toUpperCase() +". You have successfully registered with Octagon Africa. It has been sent to our team and it is awaiting Approval.Incase of any queries contact support@octagonafrica.com "
-                                });
-                                isRegistering = false;
-                                registrationStep = 0;
-                                user = {};
-                                req.session.destroy((err) => {
-                                    if(err) {
-                                      console.log(err);
-                                    } else {
-                                      console.log('Session unset');
-                                    }
-                                  });
-                                
-                                console.log(response.statusCode)
-                                
-                            } else if ([201].includes(response.statusCode)) {
-                                console.log(response.statusCode);
-                                isRegistering = false;
-                                registrationStep = 0;
-                                user = {};
-                            }else if ([400].includes(response.statusCode)) {
-                                console.log(response.statusCode);
-                                sms.send({
-                                    to: sender,
-                                    from:'20880',
-                                    message: "Registration unsuccesfull. Invalid Details or Username Exists . Please try again Later "   
-                                });
-                                isRegistering = false;
-                                registrationStep = 0;
-                                user = {};
-                            }else if ([500].includes(response.statusCode)) {
-                                console.log(response.statusCode);
-                                sms.send({
-                                    to: sender,
-                                    from:'20880',
-                                    message: "Registration unsuccesfull. Internal Server Error. Please try again Later "     
-                                });
-                                isRegistering = false;
-                                registrationStep = 0;
-                                user = {};
-                            }
-                            else {
-                                // error code
-                                console.log(response.statusCode);
-                            }
-                        });
-                    break;
-                default:
-                    // do sthg
-                    sms.send({
-                        to: sender,
-                        from:'20880',
-                        message: "Invalid response:!!"
-                    });
-                    break;
-            }
-        }else if(isDeleting){
-           
-        }else if(isCheckingAccount){
-                
-        }else if (ResetingPassword){
+          } else {
+                const insertQuery = "INSERT INTO two_way_sms_tb (text, text_id_AT, phoneNumber, isActive, time) VALUES (@text, @textId, @phoneNumber, @isActive, @time)";
 
+                const insertRequest = new sql.Request(connection);
+                insertRequest.input('text', sql.VarChar, text);
+                insertRequest.input('textId', sql.VarChar, textId);
+                insertRequest.input('phoneNumber', sql.VarChar, phoneNumber);
+                insertRequest.input('isActive', sql.Bit, isActive);
+                insertRequest.input('time', sql.DateTime2, time);
+                insertRequest.query(insertQuery, function(insertErr, insertResults) {
+                    if (insertErr) {
+                    console.error('Error executing insertQuery: ' + insertErr.stack);
+                    sql.close();
+                    return;
+                    }
+                    console.log('INSERT successful');
+                });
+            }
+            });
+            switch (text.toLowerCase()) {
+                // case '':
+                case 'register':
+                    //reset isRegistering flag and registrationStep
+                    isRegistering = false;
+                    registrationStep = 0;
+                    sms.send(register.newCustomer(sender));
+                    sms.send(register.enterId(sender));
+                    //set a flag to indicate that the user is in the process of registering
+                    isRegistering = true;         
+                    //request for ID number
+                    registrationStep = 2;
+                    const status = "isRegistering";
+                    const phoneNumber = sender;
+                    const messagingStep= "2";
+                    sql.connect(config, function(err) {
+                        const request = new sql.Request();
+                        const updateRegister1 = `UPDATE two_way_sms_tb SET status = @status, messagingStep = @messagingStep WHERE phoneNumber = @phoneNumber AND time = (
+                            SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumber )`;
+                        request.input('status', sql.VarChar, status);
+                        request.input('messagingStep', sql.VarChar, messagingStep);
+                        request.input('phoneNumber', sql.VarChar, phoneNumber);
+                        request.query(updateRegister1, function(err, results) {
+                          if (err) {
+                            console.error('Error executing query: ' + err.stack);
+                            return;
+                          }
+                          console.log('UPDATE successful');
+                          sql.close();
+                        });
+                      });
+                    break;
+                    ///other Cases
+                    // case 'balance':
+                    // messageToCustomer = 'Hello Our Dear Esteemed Customer, Welcome to Octagon Services. Enter your 4 digit pin - balance ';
+                    // sms.send({
+                    //     to: sender,
+                    //     from:'20880',
+                    //     message: messageToCustomer
+                    // });
+                    // break;
+                    case 'accounts':
+                        isCheckingAccount=false;
+                        accountStep=0;
+                        sms.send(account.welcomeMessageAccount(sender));
+                        sms.send(account.provideUserName(sender));
+                        isCheckingAccount=true;
+                        accountStep=2;
+                        break;
+                    case 'rate':
+                        messageToCustomer = 'Hello Our Dear Esteemed Customer, Welcome to Octagon Services. Enter your 4 digit pin - rate';
+                        sms.send({
+                            to: sender,
+                            from:'20880',
+                            message: messageToCustomer
+                        });
+                        
+                        break;
+                    case 'delete':
+                        isDeleting=false;
+                        deletingStep=0;
+                        messageToCustomer = 'Hello Our Dear Esteemed Customer, Welcome to Octagon Services.';
+                        
+                        sms.send({
+                            to: sender,
+                            from:'20880',
+                            message: messageToCustomer
+                        });
+                       
+                        sms.send(register.enterId(sender));
+                        isDeleting =true;
+                        deletingStep=2;
+                        break;
+                    case 'reset':
+                        ResetingPassword=false;
+                        resetStep=0;
+                        sms.send(reset.welcomeMessage(sender));
+                        sms.send(reset.enterEmail(sender));
+                        ResetingPassword=true;
+                        resetStep=2;
+                        
+                        break;
+                      
+                    default:
+                        messageToCustomer = 'Welcome To Octagon Africa you can access our services by sending the word register,save, balance,statement,products';
+                        sms.send({
+                            to: sender,
+                            from:'20880',
+                            message: messageToCustomer
+                        });
+                        break;
+                }
+      }
+      
+      function handleRegister(text, sender, messagingStep) {
+        switch (messagingStep) {
+          case '1':
+            // Handle step 1 of registration process
+            break;
+          case '2':
+
+            // Handle step 2 of registration process
+            break;
+          // ...
+          default:
+            console.log('Unknown registration step: ' + messagingStep);
+            break;
         }
+      }
+      
+      function handleDelete(text, sender, messagingStep) {
+        switch (messagingStep) {
+          case '1':
+            // Handle step 1 of delete process
+            break;
+          case '2':
+            // Handle step 2 of delete process
+            break;
+          // ...
+          default:
+            console.log('Unknown delete step: ' + messagingStep);
+            break;
+        }
+      }
+      
+      function handleAccountCheck(text, sender, messagingStep) {
+        switch (messagingStep) {
+          case '1':
+            // Handle step 1 of account check process
+            break;
+          case '2':
+            // Handle step 2 of account check process
+            break;
+          // ...
+          default:
+            console.log('Unknown account check step: ' + messagingStep);
+            break;
+        }
+      }
+      
+      function handlePasswordReset(text, sender, messagingStep) {
+        switch (messagingStep) {
+          case '1':
+            // Handle step 1 of password reset process
+            break;
+          case '2':
+            // Handle step 2 of password reset process
+            break;
+          // ...
+          default:
+            console.log('Unknown password reset step: ' + messagingStep);
+            break;
+        }
+      }
+      
+    
         res.send("Webhook received");
 });   
