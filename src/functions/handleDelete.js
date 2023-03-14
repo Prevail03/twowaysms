2f9e155aad95784713d3326b7ee4445f5b74b69a
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-03-14 10:34:26 +0300

    Update

--- src/functions/handleDelete.js
+++ src/functions/handleDelete.js
@@ -45,6 +45,7 @@ function handleDelete(text, sender, messagingStep, phoneNumber, config, sms, reg
             deletingStep=3;
             const statusPasswordDel = "isDeleting";
             const phoneNumberPasswordDel = phoneNumber;
+            console.log(phoneNumberPasswordDel);
             const messagingStepPasswordDel= "3";
             sql.connect(config, function(err) {
                 const request = new sql.Request();
