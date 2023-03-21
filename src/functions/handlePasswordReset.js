371a3ed2620eca8a284146a6c95f147acc221224
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-03-21 13:52:54 +0300

    Update

--- src/functions/handlePasswordReset.js
+++ src/functions/handlePasswordReset.js
@@ -56,6 +56,7 @@ function handlePasswordReset(text, sender, messagingStep, sms, reset, config, te
                   const statusCurrentPass = "ResetingPassword";
                   const phoneNumberCPass = sender;
                   const textIDCPass = textIDAT;
+                  console.log("statusCurrentPass" +statusCurrentPass+ "PhoneNumber" +phoneNumberCPass+ "TextIDAT" +textIDATIDCPass);
                   // Bind the values to the parameters
                   request.input('statusCurrentPass', sql.NVarChar(50), statusCurrentPass);
                   request.input('phoneNumberCPass', sql.NVarChar(50), phoneNumberCPass);
