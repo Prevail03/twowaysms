32b22d29bdff8e34c2b525f86580baa5be675d77
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-05-05 14:57:43 +0300

    update

--- src/functions/incomingmessage.js
+++ src/functions/incomingmessage.js
@@ -69,14 +69,8 @@ function handleIncomingMessage(textMessage, sender, textId, phoneNumber, config,
                     switch (textMessage.toLowerCase()) {
                         
                         case 'pension':{
-                            // sms.send(register.newCustomer(sender));
-                            // sms.send(register.enterId(sender));
-                            messageToCustomer = 'Dear Esteemed Customer, Welcome to Octagon Africa. To complete the registration process, please provide us with the following information.';
-                            sms.send({
-                                to: sender,
-                                from:'24123',
-                                message: messageToCustomer
-                            });
+                            sms.send(register.newCustomer(sender));
+                            sms.send(register.enterId(sender));
                             const status = "isRegistering";
                             const phoneNumber = sender;
                             const messagingStep= "2";
