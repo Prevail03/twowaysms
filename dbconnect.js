b449c275381b2adb0f72472a40655da291f33946
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-05-12 01:32:44 +0300

    DB

--- dbconnect.js
+++ dbconnect.js
@@ -1,13 +1,16 @@
 const config = {
-    user: 'sa',
-    password: '0ct@g0n2015?!',
-    server: '35.246.5.59',
-    database: 'MYDB',
-    options: {
-      encrypt: false,
-      trustServerCertificate: true,
-    }
+  user: 'sa',
+  password: '0ct@g0n2015?!',
+  server: '35.246.5.59',
+  database: 'MYDB',
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+    requestTimeout: 30000 // 30 seconds
+  }
 };
+
 module.exports = config;
+
 
 
