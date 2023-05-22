fd813acbcdedd1923a3b204a08e8dcf952fca953
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-05-22 20:17:32 +0300

    Update

--- src/functions/incomingmessage.js
+++ src/functions/incomingmessage.js
@@ -1,5 +1,4 @@
 const sql = require('mssql');
-var Client = require('node-rest-client').Client;const validateId = require('../validateId');
 const handleRegister = require('./handleRegister');
 const handleDelete = require('./handleDelete');
 const handleAccountCheck = require('./handleAccountCheck');
@@ -26,8 +25,32 @@ function handleIncomingMessage(textMessage, sender, textId, phoneNumber, config,
             if (checkResults.recordset.length > 0) {
                 // Existing user of two-way SMS
                 console.log('Existing user of two-way SMS');
-                // ... Handle existing user logic ...
-
+                // ... Handle existing user in two_way_sms_tb logic ...
+                //user exists check which procces and step cc
+                console.log('User Exists');
+                const status = checkResults.recordset[0].status;
+                const messagingStep = checkResults.recordset[0].messagingStep;
+                const textIDAT = checkResults.recordset[0].text_id_AT; ///text Id from Africas Talking
+                console.log(textIDAT);
+                console.log(status+" "+messagingStep);
+                switch (status) {
+                    case 'isRegistering':
+                        handleRegister(textMessage, sender, messagingStep ,sms, register, config, phoneNumber, textIDAT, LinkID);
+                        break;
+                    case 'isDeleting':
+                        handleDelete(textMessage, sender, messagingStep, config, sms, register , textIDAT, LinkID);
+                        break; 
+                    case 'isCheckingAccount':
+                        handleAccountCheck(textMessage, sender, messagingStep, sms, account, config,textIDAT, LinkID);
+                        break;
+                    case 'ResetingPassword':
+                        handlePasswordReset(textMessage, sender, messagingStep, sms, reset, config , textIDAT, LinkID);
+                        break;
+                    default:
+                        sms.sendPremium(register.defaultMessage(sender,LinkID));
+                        console.log('Unknown status: ' + status);
+                    break;
+                }
             } else {
                 // Does not exist in two_way_sms_tb
                 const checkIfExistsQuerySysUsers = "SELECT TOP 1 * FROM sys_users_tb WHERE user_mobile = @phoneNumber OR user_phone = @phoneNumber";
@@ -42,13 +65,33 @@ function handleIncomingMessage(textMessage, sender, textId, phoneNumber, config,
                     if (checkResultsSysUsers.recordset.length > 0) {
                         // Record exists in sys_users_tb
                         console.log('Record exists in sys_users_tb');
+                        sms.sendPremium(register.menuMessage(sender,LinkID));
                         // ... Handle existing record logic ...
 
                     } else {
-                        // Record does not exist in sys_users_tb
+                        // Record does not exist in sys_users_tb == a new conversion
+                        sms.sendPremium(register.menuMessage(sender,LinkID));
                         console.log('Start Registration Process');
                         // ... Handle registration process logic ...
-
+                        const status = "isRegistering";
+                        const phoneNumber = sender;
+                        const messagingStep= "2";
+                        const insertQuery = "INSERT INTO two_way_sms_tb (text, text_id_AT, status, messagingStep,phoneNumber, isActive) VALUES (@text, @text_id_AT,@status, @messagingStep, @phoneNumber, @isActive)";
+                        const insertRequest = new sql.Request(connection);
+                        insertRequest.input('text', sql.VarChar, textMessage);
+                        insertRequest.input('text_id_AT', sql.VarChar, textId);
+                        insertRequest.input('status', sql.VarChar, status);
+                        insertRequest.input('messagingStep', sql.VarChar, messagingStep);
+                        insertRequest.input('phoneNumber', sql.VarChar, phoneNumber);
+                        insertRequest.input('isActive', sql.Bit, 1);
+                        insertRequest.query(insertQuery, function(insertErr, insertResults) {
+                            if (insertErr) {
+                                console.error('Error executing insertQuery: ' + insertErr.stack);
+                                sql.close();
+                                return;
+                            }
+                            console.log(" New User inserted successfully");
+                        });
                     }
                     connection.close();
                 });
