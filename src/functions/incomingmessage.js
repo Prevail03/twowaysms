747479b7215f2c97e95d940dfc209058aa0822eb
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-05-22 22:01:04 +0300

    Update

--- src/functions/incomingmessage.js
+++ src/functions/incomingmessage.js
@@ -120,7 +120,6 @@ function handleIncomingMessage(textMessage, sender, textId, phoneNumber, config,
                         connection.close();
                     });
                 }
-                
             }
         });
     });
