a9b4e7fef24d1096d8555511488f6bb44af8228f
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-05-26 09:32:35 +0300

    update

--- src/functions/handleAccountCheck.js
+++ src/functions/handleAccountCheck.js
@@ -30,45 +30,48 @@ function handleAccountCheck(textMessage, sender, messagingStep, sms, account, co
         case 4:
             const phoneNumberDescription = sender;
             const accountDescription = textMessage;
-            let textDescription = "";
             const textIDATDescription = textIDAT;
+            let textDescription = "";
             sql.connect(config, function(err, connection) {
-                if (err) {
-                    console.error('Error connecting to database: ' + err.stack);
-                    return;
+              if (err) {
+                console.error('Error connecting to database: ' + err.stack);
+                return;
+              }
+              console.log('Connected to database');
+            
+              const checkIfExistsQuery = "SELECT TOP 1 * FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription AND isActive = 1 AND status = 'isCheckingAccount' AND time = (SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription)";
+              const checkIfExistsRequest = new sql.Request(connection);
+              checkIfExistsRequest.input('phoneNumberDescription', sql.VarChar, phoneNumberDescription);
+              checkIfExistsRequest.query(checkIfExistsQuery, function(checkErr, checkResults) {
+                if (checkErr) {
+                  console.error('Error executing checkIfExistsQuery: ' + checkErr.stack);
+                  connection.close();
+                  return;
                 }
-                console.log('Connected to database');
-    
-                const checkIfExistsQuery = "SELECT TOP 1 * FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription AND isActive = 1 AND status = 'isCheckingAccount' AND time = (SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription)";
-                const checkIfExistsRequest = new sql.Request(connection);
-                checkIfExistsRequest.input('phoneNumberDescription', sql.VarChar, phoneNumberDescription);
-                checkIfExistsRequest.query(checkIfExistsQuery, function(checkErr, checkResults) {
-                    if (checkErr) {
-                        console.error('Error executing checkIfExistsQuery: ' + checkErr.stack);
-                        connection.close();
-                        return;
-                    }
-                    if (checkResults.recordset.length > 0) {
-                        const allAccounts = checkResults.recordset[0].allAccounts;
-                        const accountsArray = allAccounts.split(',')
-                          .map(account => account.trim().replace(/^\d+\.\s*/, ''))
-                          .filter(account => account !== '');
-                        
-                        console.log("Array count:", accountsArray.length);
-                        if (accountDescription >= 1 && accountDescription <= accountsArray.length) {
-                          const selectedAccount = accountsArray[accountDescription - 1];
-                          console.log("Selected account:", selectedAccount);
-                          textDescription=selectedAccount
-                        } else {
-                          console.log("Invalid account description");
-                        }
-                    
-                    }else{
-                        console.log('Record does not exist');
-                    }
-                });
+                if (checkResults.recordset.length > 0) {
+                  const allAccounts = checkResults.recordset[0].allAccounts;
+                  const accountsArray = allAccounts.split(',')
+                    .map(account => account.trim().replace(/^\d+\.\s*/, ''))
+                    .filter(account => account !== '');
+            
+                  console.log("Array count:", accountsArray.length);
+                  if (accountDescription >= 1 && accountDescription <= accountsArray.length) {
+                    const selectedAccount = accountsArray[accountDescription - 1];
+                    console.log("Selected account:", selectedAccount);
+                    textDescription = selectedAccount;
+                    console.log("Account Description: " + textDescription);
+                  } else {
+                    console.log("Invalid account description");
+                  }
+                } else {
+                  console.log('Record does not exist');
+                }
+            
+                // You can use textDescription here or pass it to other functions as needed
+                // ...
+              });
             });
-            console.log("Selected account:", selectedAccount);
+            
             console.log("Account Description:" +textDescription);
             // updateDescription(phoneNumberDescription, textDescription, textIDATDescription, sender, config, textIDAT, sms, account, LinkID);
             break;
