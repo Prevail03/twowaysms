137f052e4df68271e61c0eb8ae29e2db788c2016
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-06-08 10:42:24 +0300

    Update

--- src/functions/Database/accountDB.js
+++ src/functions/Database/accountDB.js
@@ -344,7 +344,7 @@ function updatePassword(phoneNumberPassword, textPassword, textIDATPassword, sen
                       let loginAttempts = checkResultsSysUsers.recordset[0].loginAttemptsCounter;
                       loginAttempts = parseInt(loginAttempts, 10);
                       console.log(loginAttempts);
-                      if (loginAttempts <= 3) {
+                      if (loginAttempts <= 2) {
                         const loginAttemptNumber = loginAttempts + 1;
                         const request = new sql.Request();
                         const statuserror404 = "isCheckingAccount";
