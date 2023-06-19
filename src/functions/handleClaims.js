cee88df21f2aed517f52964314c17f070e265c9f
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-06-19 03:36:05 +0300

    Update

--- src/functions/handleClaims.js
+++ src/functions/handleClaims.js
@@ -20,43 +20,43 @@ function  handleClaims(textMessage, sender, messagingStep, sms,  config, textIDA
       const textIDATDescription = textIDAT;
       let textDescription = "";
       sql.connect(config, function(err, connection) {
-      if (err) {
-          console.error('Error connecting to database: ' + err.stack);
-          return;
-      }
-      console.log('Connected to database');
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            return;
+        }
+        console.log('Connected to database');
 
-      const checkIfExistsQuery = "SELECT TOP 1 * FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription AND isActive = 1 AND status = 'isMakingClaims' AND time = (SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription)";
-      const checkIfExistsRequest = new sql.Request(connection);
-      checkIfExistsRequest.input('phoneNumberDescription', sql.VarChar, phoneNumberDescription);
-      checkIfExistsRequest.query(checkIfExistsQuery, function(checkErr, checkResults) {
-          if (checkErr) {
-          console.error('Error executing checkIfExistsQuery: ' + checkErr.stack);
-          connection.close();
-          return;
-          }
-          if (checkResults.recordset.length > 0) {
-              const allAccounts = checkResults.recordset[0].allAccounts;
-              const accountsArray = allAccounts.split(',')
-                  .map(account => account.trim().replace(/^\d+\.\s*/, ''))
-                  .filter(account => account !== '');
+        const checkIfExistsQuery = "SELECT TOP 1 * FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription AND isActive = 1 AND status = 'isMakingClaim' AND time = (SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription)";
+        const checkIfExistsRequest = new sql.Request(connection);
+        checkIfExistsRequest.input('phoneNumberDescription', sql.VarChar, phoneNumberDescription);
+        checkIfExistsRequest.query(checkIfExistsQuery, function(checkErr, checkResults) {
+        if (checkErr) {
+        console.error('Error executing checkIfExistsQuery: ' + checkErr.stack);
+        connection.close();
+        return;
+        }
+        if (checkResults.recordset.length > 0) {
+            const allAccounts = checkResults.recordset[0].allAccounts;
+            const accountsArray = allAccounts.split(',')
+                .map(account => account.trim().replace(/^\d+\.\s*/, ''))
+                .filter(account => account !== '');
 
-              console.log("Array count:", accountsArray.length);
-              if (accountDescription >= 1 && accountDescription <= accountsArray.length) {
-                  const selectedAccount = accountsArray[accountDescription - 1];
-                  console.log("Selected account:", selectedAccount);
-                  textDescription = selectedAccount;
-                  console.log("Account Description: " + textDescription);
-                  updateDescription(phoneNumberDescription, textDescription, textIDATDescription, sender, config, textIDAT, sms, account, LinkID);
-              } else {
-                  console.log("Invalid account description");
-                  sms.sendPremium(claims.invalidResponse(sender, LinkID));
-              }
-          } else {
-          console.log('Record does not exist');
-          }
+            console.log("Array count:", accountsArray.length);
+            if (accountDescription >= 1 && accountDescription <= accountsArray.length) {
+                const selectedAccount = accountsArray[accountDescription - 1];
+                console.log("Selected account:", selectedAccount);
+                textDescription = selectedAccount;
+                console.log("Account Description: " + textDescription);
+                updateDescription(phoneNumberDescription, textDescription, textIDATDescription, sender, config, textIDAT, sms, account, LinkID);
+            } else {
+                console.log("Invalid account description");
+                sms.sendPremium(claims.invalidResponse(sender, LinkID));
+            }
+        } else {
+        console.log('Record does not exist');
+        }
       });
-      });
+    });
     break;
     case 3:
 
