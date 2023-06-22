f6a683e412254b0ec002aa706f5192c427116501
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-06-22 12:43:44 +0300

    Update

--- src/functions/Database/accountDB.js
+++ src/functions/Database/accountDB.js
@@ -479,6 +479,7 @@ function updateDescription(phoneNumberDescription, textDescription, textIDATDesc
     const updateAccounts = `UPDATE two_way_sms_tb SET status = 'isCheckingAccount', messagingStep= '5', description = @textDescription, memberID = @memberID WHERE phoneNumber = @phoneNumberDescription AND text_id_AT = @textIDATDescription AND time = (SELECT MAX(time) FROM two_way_sms_tb WHERE phoneNumber = @phoneNumberDescription)`;
     request.input('phoneNumberDescription', sql.NVarChar, phoneNumberDescription);
     request.input('textIDATDescription', sql.NVarChar, textIDATDescription);
+    request.input('memberID', sql.NVarChar, memberID);
     request.input('textDescription', sql.NVarChar, textDescription);
     request.query(updateAccounts, function (err, results) {
       if (err) {
