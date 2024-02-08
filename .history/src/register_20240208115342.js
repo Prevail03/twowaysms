a01bc1fda3ea416923c2853e9166c863c732b5b8
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2024-02-08 11:55:06 +0300

    Removing emojis

--- .history/src/register_20240208115342.js
+++ .history/src/register_20240208115342.js
@@ -79,7 +79,7 @@ const register = {
     wrongMenuValue: (sender, LinkID) => ({
         to: sender,
         from:'24123',
-        message: "Coming soon...",
+        message: "Coming soon...🎆😊",
         bulkSMSMode: 0,
         keyword: 'pension',
         linkId: LinkID
